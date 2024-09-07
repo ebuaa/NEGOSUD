bf82d89ec1ca6a36ef18fb7c955927034b420fb1
Anastasios Statiris <tasosstatiris@outlook.fr>
2024-09-07 10:44:40 +0200

    corrected a mistake

--- NEGOSUD/Models/Entities/OrderDetail.cs
+++ NEGOSUD/Models/Entities/OrderDetail.cs
@@ -4,7 +4,7 @@
     {
         public int OrderDetailID { get; set; }
         public int OrderID { get; set; }
-        public Order Order { get; set;
+        public Order Order { get; set; }
         public int ProductID { get; set; }
         public Product Product { get; set; }
         public int Quantity { get; set; }
