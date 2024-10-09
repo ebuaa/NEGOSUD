0df439ad96eade752b8936633e11c3170af91966
Anastasios Statiris <tasosstatiris@outlook.fr>
2024-10-09 13:20:53 +0200

    OrderCreate Page works and updates the cart number, Cart page Read Works, Need to add Delete as a minimum

--- NEGOSUD/Controllers/OrderController.cs
+++ NEGOSUD/Controllers/OrderController.cs
@@ -32,5 +32,65 @@ namespace NEGOSUD.Controllers
 
 			return View(model);
 		}
+
+		[HttpPost]
+		[Authorize]
+        public async Task<IActionResult> AddDetail(int productID, int StockQuantity)
+        {
+            var product = await _context.Products.FindAsync(productID);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            // Retrieve or Create the OrderViewModel from the session state
+            var model = HttpContext.Session.Get<OrderViewModel>("OrderViewModel") ?? new OrderViewModel
+            {
+                OrderDetails = new List<OrderDetailViewModel>(),
+                Products = await _context.Products.ToListAsync()
+            };
+
+            // Check if the product is already in the order
+            var existingDetail = model.OrderDetails.Find(od => od.ProductID == productID);
+
+            // If the product is already in the order, update the quantity
+            if (existingDetail != null)
+            {
+                existingDetail.Quantity += StockQuantity;
+            }
+            else
+            {
+                model.OrderDetails.Add(new OrderDetailViewModel
+                {
+                    ProductID = productID,
+                    ProductName = product.Name,
+                    Quantity = StockQuantity,
+                    Price = product.PricePerUnit
+                });
+            }
+
+            // Update the total amount
+            model.TotalAmount = model.OrderDetails.Sum(od => od.Price * od.Quantity);
+
+            // Save updated OrderViewModel in the session state
+            HttpContext.Session.Set("OrderViewModel", model);
+
+            // Redirect back to the Create view to show or update the order
+            return RedirectToAction(nameof(Create));
+        }
+
+        [HttpGet]
+		[Authorize]
+		public async Task<IActionResult> Cart()
+		{
+			//Retrieve the OrderViewModel from the session state
+			var model = HttpContext.Session.Get<OrderViewModel>("OrderViewModel");
+
+			if (model == null || model.OrderDetails.Count == 0)
+			{
+				return RedirectToAction(nameof(Create));
+			}
+			return View(model);
+		}
 	}
 }
