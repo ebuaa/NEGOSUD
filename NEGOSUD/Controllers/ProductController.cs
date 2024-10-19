aaad90abfeabc6c9b3c51443f3cfe7c590b526c9
Anastasios Statiris <tasosstatiris@outlook.fr>
2024-10-19 21:51:17 +0200

    Word search and categories search works

---
+++ NEGOSUD/Controllers/ProductController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using NEGOSUD.Data;
+using NEGOSUD.Models.Entities;
+
+namespace NEGOSUD.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Products
+        public async Task<IActionResult> Index(string? search, string? category)
+        {
+            //Basic query
+            var productsQuery = _context.Products
+                .Include(p => p.Category)
+                .Include(p => p.Supplier)
+                .AsQueryable();
+
+            //Search
+            if (!string.IsNullOrEmpty(search))
+            {
+                productsQuery = productsQuery.Where(p => p.Name.Contains(search) || p.Description.Contains(search) ||p.Category.Name.Contains(search));
+            }
+
+            //Category filter 
+            if (!string.IsNullOrEmpty(category))
+            {
+                productsQuery = productsQuery.Where(p => p.Category.Name == category);
+            }
+            //execute query
+            var products = await productsQuery.ToListAsync();
+
+            //return view
+            ViewBag.Search = search;
+            ViewBag.Category = category;
+            return View(products);
+        }
+
+        // GET: Products/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products
+                .Include(p => p.Category)
+                .Include(p => p.Supplier)
+                .FirstOrDefaultAsync(m => m.ProductID == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+    }
+}
