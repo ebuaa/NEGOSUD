ac3a7863dbb6489f862460d4cd904dc3d4fd2075
Anastasios Statiris <tasosstatiris@outlook.fr>
2024-10-20 11:48:50 +0200

    front-end corrections

--- NEGOSUD/wwwroot/js/site.js
+++ NEGOSUD/wwwroot/js/site.js
@@ -15,24 +15,23 @@
     };
     spinner(0);
 
-    // Product Quantity
-    $('.quantity button').on('click', function () {
-        var button = $(this);
-        var oldValue = button.parent().parent().find('input').val();
-        if (button.hasClass('btn-plus')) {
-            var newVal = parseFloat(oldValue) + 1;
-        } else {
-            if (oldValue > 0) {
-                var newVal = parseFloat(oldValue) - 1;
+
+    // Fixed Navbar
+    $(window).scroll(function () {
+        if ($(window).width() < 992) {
+            if ($(this).scrollTop() > 55) {
+                $('.fixed-top').addClass('shadow');
             } else {
-                newVal = 0;
+                $('.fixed-top').removeClass('shadow');
+            }
+        } else {
+            if ($(this).scrollTop() > 55) {
+                $('.fixed-top').addClass('shadow').css('top', -55);
+            } else {
+                $('.fixed-top').removeClass('shadow').css('top', 0);
             }
         }
-        button.parent().parent().find('input').val(newVal);
     });
 
 
-  
-
 })(jQuery);
-
